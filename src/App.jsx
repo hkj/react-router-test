9e83940038a6336965276a4e34bd7232fcf5f4ad
hocnodon <315029+hkj@users.noreply.github.com>
2024-06-11 23:31:37 +0900

    gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Page1 } from "./components/Page1";
+import { Page2 } from "./components/Page2";
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/react-router-test/">
       <div className="App">
         <Link to="/">Home</Link>
         <br />
@@ -9,8 +12,12 @@ function App() {
         <br />
         <Link to="Page2">Page2</Link>
       </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
