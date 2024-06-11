0878d374b7783386d60505629d6f0f440f04cfd2
hocnodon <315029+hkj@users.noreply.github.com>
2024-06-12 02:34:08 +0900

    homepage again

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import { Home } from "./components/Home";
 import { Page1 } from "./components/Page1";
 import { Page2 } from "./components/Page2";
 function App() {
+  const APP_NAME = "react-router-test";
   return (
-    // <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/react-router-test"}>
+    <BrowserRouter>
       <div className="App">
-        <Link to="/">Home</Link>
+        <Link to={`${APP_NAME}/`}>Home</Link>
         <br />
-        <Link to="/Page1">Page1</Link>
+        <Link to={`${APP_NAME}/page1`}>Page1</Link>
         <br />
-        <Link to="Page2">Page2</Link>
+        <Link to={`${APP_NAME}/page2`}>Page2</Link>
       </div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page1 />} />
-        <Route path="/page2" element={<Page2 />} />
+        <Route path={`${APP_NAME}/`} element={<Home />} />
+        <Route path={`${APP_NAME}/page1`} element={<Page1 />} />
+        <Route path={`${APP_NAME}/page2`} element={<Page2 />} />
       </Routes>
     </BrowserRouter>
   );
